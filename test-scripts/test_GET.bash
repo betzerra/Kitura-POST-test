8c199e789cf62f1dac3c8085b8db839db5bd091f
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2022-10-20 17:38:33 -0300

    Fixed: scripts

--- test-scripts/test_GET.bash
+++ test-scripts/test_GET.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-curl "http://localhost:8080/"
-curl "http://localhost:8080/"
+curl "http://localhost:8080/" &
+curl "http://localhost:8080/" &
+wait
--- test-scripts/test_POST.bash
+++ test-scripts/test_POST.bash
@@ -9,3 +9,5 @@ curl -i -X "POST" "http://localhost:8080/" \
      -H 'Authorization: ' \
      -H 'Content-Type: application/json' \
      -d $'{}' &
+
+wait
